207d10cc2b443ea8e7c98e58d6e44ce98866c27c
harinireddycheemala <harinireddycheemala@gmail.com>
2026-03-14 14:53:29 +0530

    Create AllergyChat.jsx

---
+++ src/pages/AllergyChat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AllergyChat() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center", border: "5px solid red", backgroundColor: "yellow" }}>
+      <h1>HELLO WORLD - ALLERGY PAGE</h1>
+      <p>If you see this, the file is working.</p>
+      <button style={{ padding: "20px", fontSize: "20px" }}>CLICK ME</button>
+    </div>
+  );
+}
