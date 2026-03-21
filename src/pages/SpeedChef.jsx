bf8f14c2bf8bb2be65b6785b8285fd1dea30113d
harinireddycheemala <harinireddycheemala@gmail.com>
2026-03-21 13:17:59 +0530

    Update SpeedChef.jsx

--- src/pages/SpeedChef.jsx
+++ src/pages/SpeedChef.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+ import React, { useState, useEffect } from "react";
 
 const SpeedChef = () => {
   const ingredients = ["🍅 Tomato", "🧀 Cheese", "🥬 Lettuce", "🥩 Meat", "🍞 Bread", "🥕 Carrot", "🧅 Onion", "🥔 Potato"];
